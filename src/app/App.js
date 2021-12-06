5f2a8913075f39b497f891a1c7f71d7636733421
Leo <leo.cabral1016@gmail.com>
2021-12-06 01:42:38 -0300

    frontend 1

---
+++ src/app/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                {/* NAVIGATION */}
+                <nav className="light-blue darken-4">
+                    <div className="container">
+                        <a className="brand-logo" href="/">MERN Stack</a>
+                    </div>
+                </nav>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="col s5">
+                            <div className="card">
+                                <div className="card-content">
+                                    <form>
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <input type="text" placeholder="Task Title"></input>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <textarea placeholder="Task Description"></textarea>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col s7">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
